e4ce904d495f1efc43b6cb8556af419b561312ef
jamyu <jamyu@umich.edu>
2024-10-19 19:19:12 -0400

    createRoutesFromElements

--- src/main.jsx
+++ src/main.jsx
@@ -6,46 +6,49 @@ import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import EditContact, {action as editAction} from "./routes/edit";
 import {action as destroyAction} from "./routes/destroy"
 
 import Index from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    loader: rootLoader,
-    action: rootAction,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {index: true, element: <Index />},
-          {
-            path: "contacts/:contactID",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-            //errorElement: <div>Oops! There was an error.</div>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 import App from "./App";
 
